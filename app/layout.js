7e79c2980f9781f89b8b2a24f3eca7a9c8305706
Mohnish <mohnishkumar57@gmail.com>
2024-07-20 21:20:40 +0530

    bug fixing

--- app/layout.js
+++ app/layout.js
@@ -46,12 +46,12 @@ export default function RootLayout({ children }) {
         </AuthProvider>
         {/* <BootstrapClient /> */}
         <Script src="assets/vendor/aos/aos.js" />
-        <Script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy='beforeInteractive'/>
+        <Script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"/>
         <Script src="assets/vendor/glightbox/js/glightbox.min.js" />
         <Script src="assets/vendor/isotope-layout/isotope.pkgd.min.js" />
         <Script src="assets/vendor/swiper/swiper-bundle.min.js"/>
         {/* <!-- Template Main JS File --> */}
-        <Script src="assets/js/main.js" strategy='beforeInteractive'/>
+        <Script src="assets/js/main.js"/>
       </body>
     </html>
   );
