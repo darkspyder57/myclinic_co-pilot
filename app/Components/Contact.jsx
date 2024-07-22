3e6344c36e4434601eb177715ad54bcc0dcdc65b
Mohnish <mohnishkumar57@gmail.com>
2024-07-22 23:25:00 +0530

    addded contact details and fixed bugs

--- app/Components/Contact.jsx
+++ app/Components/Contact.jsx
@@ -75,19 +75,21 @@ export default function Contact() {
                             <div className="address">
                                 <i className="bi bi-geo-alt"></i>
                                 <h4>Location:</h4>
-                                <p>A108 Adam Street, New York, NY 535022</p>
+                                <p>91 Springboard, Creaticity, Yerwada, Pune, India. 411006.</p>
                             </div>
 
                             <div className="email">
                                 <i className="bi bi-envelope"></i>
                                 <h4>Email:</h4>
-                                <p>info@example.com</p>
+                                <p>info@ideationpeople.com</p>
                             </div>
 
                             <div className="phone">
                                 <i className="bi bi-phone"></i>
                                 <h4>Call:</h4>
-                                <p>+1 5589 55488 55s</p>
+                                <p>+1 415 800 4211</p>
+                                <p>+44 33 0001 2448</p>
+                                <p>+91 87793 00073</p>
                             </div>
 
                         </div>
