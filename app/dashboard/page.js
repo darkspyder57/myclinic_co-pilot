5f3ed16bcba58d08e6e832de99170a3408367bf4
Mohnish <mohnishkumar57@gmail.com>
2024-07-25 22:40:57 +0530

    bug fixed

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -8,10 +8,110 @@ import Link from "next/link";
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import withAuth from "../withAuth";
+import { useEffect } from "react";
 const AdminDashboard =()=> {
 
     const [view, setView] = useState('contact');
     const {logout} = useAuth();
+
+    useEffect(() => {
+        const select = (selector, all = false) => 
+          all ? Array.from(document.querySelectorAll(selector)) : document.querySelector(selector);
+    
+        const mobileNavToggleBtn = select(".mobile-nav-toggle");
+        if (mobileNavToggleBtn) {
+          mobileNavToggleBtn.addEventListener("click", () => {
+            const navbar = select("#navbar");
+            if (navbar) {
+              navbar.classList.toggle("navbar-mobile");
+            }
+            mobileNavToggleBtn.classList.toggle("bi-list");
+            mobileNavToggleBtn.classList.toggle("bi-x");
+          });
+        }
+    
+        let navbarlinks = select('#navbar .scrollto', true);
+        const navbarlinksActive = () => {
+          let position = window.scrollY + 200;
+          navbarlinks.forEach(navbarlink => {
+            const anchor = navbarlink;
+            if (!anchor.hash) return;
+            const section = document.querySelector(anchor.hash);
+            if (!section) return;
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+              anchor.classList.add('active');
+            } else {
+              anchor.classList.remove('active');
+            }
+          });
+        };
+    
+        window.addEventListener('load', navbarlinksActive);
+        window.addEventListener('scroll', navbarlinksActive);
+    
+        const dropdowns = select('.navbar .dropdown > a', true);
+        if (dropdowns) {
+          dropdowns.forEach(dropdown => {
+            dropdown.addEventListener('click', (e) => {
+              if ((select('#navbar')).classList.contains('navbar-mobile')) {
+                e.preventDefault();
+                (dropdown.nextElementSibling).classList.toggle('dropdown-active');
+              }
+            });
+          });
+        }
+    
+        const scrollto = (el) => {
+          const header = select('#header');
+          const offset = header.offsetHeight;
+          const elementPos = (select(el)).offsetTop;
+          window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+          });
+        };
+    
+        const scrollLinks = select('.scrollto', true);
+        if (scrollLinks) {
+          scrollLinks.forEach(link => {
+            link.addEventListener('click', (e) => {
+              if ((select('#navbar')).classList.contains('navbar-mobile')) {
+                (select('#navbar')).classList.remove('navbar-mobile');
+                mobileNavToggleBtn.classList.toggle("bi-list");
+                mobileNavToggleBtn.classList.toggle("bi-x");
+              }
+              if ((link).hash) {
+                e.preventDefault();
+                scrollto((link).hash);
+              }
+            });
+          });
+        }
+    
+        const selectHeader = select('#header');
+        const headerScrolled = () => {
+          if (window.scrollY > 100) {
+            selectHeader.classList.add('header-scrolled');
+            console.log("header toggled");
+          } else {
+            selectHeader.classList.remove('header-scrolled');
+          }
+        };
+    
+        if (selectHeader) {
+          window.addEventListener('load', headerScrolled);
+          window.addEventListener('scroll', headerScrolled);
+        }
+    
+        return () => {
+          window.removeEventListener('load', navbarlinksActive);
+          window.removeEventListener('scroll', navbarlinksActive);
+          if (selectHeader) {
+            window.removeEventListener('load', headerScrolled);
+            window.removeEventListener('scroll', headerScrolled);
+          }
+        };
+      }, []);
 
     return (
         <>
