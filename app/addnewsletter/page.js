0dc933a87e0457b2441a08579323655d08578c79
Mohnish <mohnishkumar57@gmail.com>
2024-08-01 12:22:58 +0530

    bug fixed

--- app/addnewsletter/page.js
+++ app/addnewsletter/page.js
@@ -7,7 +7,7 @@ export default function AddNewsletter() {
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <small id="emailHelp" class="form-text text-muted">We&apos;ll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
