1015dc1623ea4c8f609f08b6ae37ced0e9eabba5
Mohnish <mohnishkumar57@gmail.com>
2024-08-05 13:16:16 +0530

    added addnewsletter section to create newsletter in newsletter admin and also added storage bucket for images

--- app/addnewsletter/page.js
+++ app/addnewsletter/page.js
@@ -1,24 +1,86 @@
-"use client"
+"use client";
+
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useState } from "react";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from "../Firebase";
+
+
+const MyCustomUploadAdapter = (loader) => {
+    return {
+        upload: () => {
+            return new Promise((resolve, reject) => {
+                loader.file.then((file) => {
+                    const storageRef = ref(storage, `images/${file.name}`);
+                    uploadBytes(storageRef, file).then((snapshot) => {
+                        getDownloadURL(snapshot.ref).then((downloadURL) => {
+                            resolve({
+                                default: downloadURL,
+                            });
+                        }).catch((error) => {
+                            reject(error);
+                        });
+                    }).catch((error) => {
+                        reject(error);
+                    });
+                });
+            });
+        },
+    };
+};
+
+function MyCustomUploadAdapterPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        return MyCustomUploadAdapter(loader);
+    };
+}
 
 export default function AddNewsletter() {
-    return
-    <div className="justify-content-center">
-        <form>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" class="form-text text-muted">We&apos;ll never share your email with anyone else.</small>
+    const [editorData, setEditorData] = useState('');
+    return (
+        <div className="bg-info py-5">
+            <div className="container mt-4">
+                <h2 className="text-center mb-4 text-white">Create Newsletters using this Editor 🎨</h2>
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-8">
+                        <div className="card">
+                            <div className="card-body">
+                                <CKEditor
+                                    editor={ClassicEditor}
+                                    data={editorData}
+                                    config={{
+                                        extraPlugins: [MyCustomUploadAdapterPlugin],
+                                        toolbar: [
+                                            'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'insertTable', 'mediaEmbed', 'undo', 'redo', 'imageUpload'
+                                        ],
+                                        image: {
+                                            toolbar: [
+                                                'imageTextAlternative', 'imageStyle:full', 'imageStyle:side'
+                                            ]
+                                        }
+                                    }}
+                                    onChange={(event, editor) => {
+                                        const data = editor.getData();
+                                        setEditorData(data);
+                                        console.log({ event, editor, data });
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center mt-4">
+                    <div className="col-12 col-md-8">
+                        <div className="card">
+                            <div className="card-body">
+                                <h3>Content</h3>
+                                <div className="ck-content" dangerouslySetInnerHTML={{ __html: editorData }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-
+        </div>
+    );
 }
